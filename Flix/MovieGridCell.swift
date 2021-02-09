7efe29300d2d05fc72eb1544b72182f8ed30a60f
Jonathan Ballona <jballona5127@gmail.com>
2021-02-08 16:55:12 -0800

    part 2: commit includes gallery view of superheroes alongside info

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Jonathan Ballona Sanchez on 2/8/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
